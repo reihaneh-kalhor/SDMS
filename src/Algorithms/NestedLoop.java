c5c4a9fe7ee9f7d0e52c07aa293cbfffdf8cd47c
BertVanMieghem <34281226+BertVM52@users.noreply.github.com>
2020-11-23 15:19:54 +0100

    Read airport dataset and create Airport class instances

--- src/Algorithms/NestedLoop.java
+++ src/Algorithms/NestedLoop.java
@@ -13,4 +13,5 @@ package Algorithms;
 
 
 public class NestedLoop {
+
 }
--- src/TestAlgorithm.java
+++ src/TestAlgorithm.java
@@ -1,5 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
 public class TestAlgorithm {
     public static void main(String[] args) {
-        System.out.println("Testing Algorithm 1");
+
+        System.out.println("Reading dataset");
+        ArrayList<Airport> set = new ArrayList<Airport>();
+        try {
+            String[] lines = Files.readAllLines(new File("airports_lite.dat").toPath()).toArray(new String[0]); // airports_lite.dat: 3790 airports
+            for (String i : lines) {
+                Airport a = new Airport(i);
+//                a.print();
+                set.add(a);
+            }
+            set.get(0).print();
+            System.out.println("number of airports: " + set.size());
+        } catch(IOException ex) {
+            System.out.println(ex.toString());
+        }
     }
 }
