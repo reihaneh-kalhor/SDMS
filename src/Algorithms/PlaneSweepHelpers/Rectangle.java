8eba8b9e39b225fa8a5346d525ca0c6ae812c72a
Jef Jacobs <jef@MacBook-Pro-van-Jef.local>
2020-11-25 10:46:15 +0100

    Implemented Rectangle class

--- src/Algorithms/PlaneSweepHelpers/Rectangle.java
+++ src/Algorithms/PlaneSweepHelpers/Rectangle.java
@@ -2,17 +2,20 @@ package Algorithms.PlaneSweepHelpers;
 
 //TODO: Implement this class
 public class Rectangle {
-    //Store left side of Rectangle
-    public float left;
+    //Store sides of Rectangle
+    public float left, right, bottom, top;
 
     //Constructor
-    public Rectangle(float _left) {
+    public Rectangle(float _left, float _right, float _bottom, float _top){
         left = _left;
+        right = _right;
+        bottom = _bottom;
+        top = _top;
     }
 
     //Method to test if this rectangle intersects with another Rectangle
     public boolean intersects(Rectangle otherRectangle){
-        //TODO: implement intersection test
-        return true;
+        return (this.left < otherRectangle.right && this.right > otherRectangle.left &&
+                this.top > otherRectangle.bottom && this.bottom < otherRectangle.top);
     }
 }
