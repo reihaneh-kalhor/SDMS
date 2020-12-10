062fe336bebbd5c805f3b2add97f71b0ade56b55
reihaneh-kalhor <kalhor.reihaneh@gmail.com>
2020-12-10 14:13:26 +0100

    adding benchmakr for indexednestedloop

---
+++ src/Benchmarks/IndexedNestedLoopBenchmark.java
@@ -0,0 +1,69 @@
+package Benchmarks;
+
+import Algorithms.IndexedNestedLoop;
+import Database.ClientDB;
+import Italy.ItalyLocation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IndexedNestedLoopBenchmark {
+
+    private static long calculateAvgDuration(ArrayList<Long> durations){
+        long temp = 0;
+        for (int i=0; i<durations.size();i++){
+            temp += durations.get(i);
+        }
+        return temp/durations.size();
+    }
+
+    private static void printResults(int warmUp, int reps, long avg, int intersections){
+        System.out.println("--------------------------");
+        System.out.println("Detected Intersections: " + intersections);
+        System.out.println("Total runs: " + reps);
+        System.out.println("Warm-up runs: " + warmUp);
+        System.out.println("Average NestedLoop Duration: " + avg + "ms");
+        System.out.println("--------------------------");
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Long> durations = new ArrayList<>();
+        int detectedIntersections = 0;
+        int warmUp = 5;
+        int reps = 15;
+        if(args.length>0){
+            reps = Integer.parseInt(args[0]);
+            if(args.length>1){
+                warmUp = Integer.parseInt(args[1]);
+            }
+        }
+
+        //Database
+        ClientDB db = new ClientDB();
+
+        System.out.println("Reading data from Database...");
+        ArrayList<ItalyLocation> communities = db.readCommunities();
+        ArrayList<ItalyLocation> railways = db.readRailways();
+        System.out.println(" ~ done");
+
+
+        // IndexedNestedLoop object
+        IndexedNestedLoop idxnl = new IndexedNestedLoop();
+
+        //Run Benchmark
+        for (int i=0;i<reps;i++){
+            long psStartTime = System.nanoTime();
+            ArrayList<List> result = idxnl.join(communities, railways);
+            long endTime = System.nanoTime();
+            long duration = (endTime - psStartTime) / 1000000;  //divide by 1000000 to get ms, 1000000000 for sec.
+            detectedIntersections = result.size();
+            System.out.println("IndexedNestedLoop took: " + duration + "ms");
+            if (i>= warmUp){
+                durations.add(duration);
+            }
+        }
+
+        //Report Result
+        long avg = calculateAvgDuration(durations);
+        printResults(warmUp, reps, avg, detectedIntersections);
+    }
+}
